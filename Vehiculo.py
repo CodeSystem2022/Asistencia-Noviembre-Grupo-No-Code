dda93d1087701effdcec1c368eb926ed140985f4
Abelino537 <103205401+Abelino537@users.noreply.github.com>
2022-11-10 22:13:41 -0300

    AsistenciaAbel.py

--- Vehiculo.py
+++ Vehiculo.py
@@ -165,3 +165,28 @@ print(cuadrado1)
 rectangulo1 = Rectangulo(3, 6, 'Verde')
 print(f'Cálculo del área del rectángulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
+
+
+# Abel Pierna
+# Asistencia Noviembre 
+# Laboratorio 2
+# <No_Code/>
+class FiguraGeo:
+    def __init__(self, ancho, alto):
+        self._alto = alto
+        self._ancho = ancho
+    @property
+    def alto(self):
+        return self._alto
+    @alto.setter
+    def alto(self, alto):
+        self._alto = alto
+    @property
+    def ancho(self):
+        return self._ancho
+    @ancho.setter
+    def ancho(self, ancho):
+        self._ancho = ancho
+
+    def __str__(self):
+        return print(f' La Figura Geometrica tiene las siguientes proporciones [ ancho: {self._ancho}, alto: {self._alto}]')
